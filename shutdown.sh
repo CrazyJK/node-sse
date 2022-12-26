e51aab723e3640c6dbe902f9f6efff607b30745f
jongkwan-nam <jongkwan.nam@gmail.com>
2022-12-26 15:31:29 +0900

    start/stop

---
+++ shutdown.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+PID_FILE="./noti.pid"
+LOG_DIR="./log"
+SSE_NOHUP_LOG="$LOG_DIR/nohup.log"
+
+
+if [ ! -f $PID_FILE ]; then
+	echo "Notificator SSE Server is Not Running!!"
+	exit 1
+else
+	PID=`cat $PID_FILE 2>/dev/null`
+	echo "Shutting down Notificator SSE Server: $PID"
+	kill $PID 2>/dev/null
+	sleep 2
+	kill -9 $PID 2>/dev/null
+	rm -f $PID_FILE
+	echo "STOPPED `date`" >>$SSE_NOHUP_LOG
+fi
+
+exit 0
---
+++ startup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export NODE_ENV=production
+
+PID_FILE="./noti.pid"
+LOG_DIR="./log"
+SSE_NOHUP_LOG="$LOG_DIR/nohup.log"
+
+RUN_CMD="node ./app"
+
+if [ -f $PID_FILE ]; then
+	echo "Notificator SSE Server is Already Running!!"
+	exit 1
+else
+	echo "Notificator SSE Server initializing..."
+	nohup sh -c "exec $RUN_CMD >>$SSE_NOHUP_LOG 2>&1" >/dev/null &
+	echo $! > $PID_FILE
+	echo "Notificator SSE Server is running pid="`cat $PID_FILE`
+fi
+
+exit 0
